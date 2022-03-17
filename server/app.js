a73333879ef5255b15929ff813357e9bcac9b607
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-17 16:01:05 +0800

    add erro handler middlewares

--- server/app.js
+++ server/app.js
@@ -5,4 +5,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Setup error handlers
+const errorHandler = require("./handlers/errorHandler");
+app.use(errorHandler.notFound);
+app.use(errorHandler.mongoseErrors);
+if (process.env.ENV === "DEVELOPMENT") app.use(errorHandler.developmentErrors);
+else app.use(errorHandler.productionErrors);
+
 module.exports = app;
