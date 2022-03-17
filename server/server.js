29a353241f216b48952f775037e2794064016b54
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-17 16:06:05 +0800

    add database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const app = require("./app");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server listening on port ${PORT}`);
+  await mongoose.connect(process.env.DATABASE);
+  console.log(`Database connected`);
 });
