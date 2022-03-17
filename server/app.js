38daf11bd92a1773c2e358635416263e61a826be
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-17 15:56:39 +0800

    add app

--- server/app.js
+++ server/app.js
@@ -1,4 +1,8 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+module.exports = app;
