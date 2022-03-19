6879153051e0f33e0e694f651441cf11c6af35c7
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-19 19:59:25 +0800

    add register route

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,6 @@ const { catchErrors } = require("../handlers/errorHandler");
 const userController = require("../controllers/userController");
 
 router.post("/login", catchErrors(userController.login));
-router.post("/login", catchErrors(userController.register));
+router.post("/register", catchErrors(userController.register));
 
 module.exports = router;
