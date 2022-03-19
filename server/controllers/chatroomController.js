83dae902d9c997f05e979402b5b303a9600424e3
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-19 20:28:48 +0800

    add create chatroom

--- server/controllers/chatroomController.js
+++ server/controllers/chatroomController.js
@@ -1,3 +1,22 @@
-exports.createChatroom = async(req,res) => {
-    
-}
+const Chatroom = require("../models/Chatroom");
+
+exports.createChatroom = async (req, res) => {
+  const { name } = req.body;
+  const nameRegex = /^[A-Za-z\s]+$/;
+
+  if (!nameRegex.test(name)) throw "Chatroom name can contain only alphabets.";
+
+  const chatroomExists = await Chatroom.findOne({ name });
+
+  if (chatroomExists) throw "Chatroom with that name already exists!";
+
+  const chatroom = new Chatroom({
+    name,
+  });
+
+  await chatroom.save();
+  res.json({
+    message: "Chatroom created!",
+  });
+};
+
