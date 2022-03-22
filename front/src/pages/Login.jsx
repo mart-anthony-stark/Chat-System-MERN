3c78d8b6dffd78f3e32b3cec016041a117dc81c3
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-22 20:08:43 +0800

    add login ui

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Login = () => {
   return (
     <div className="card">
@@ -7,8 +5,23 @@ const Login = () => {
       <div className="cardBody">
         <div className="inputGroup">
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="abc@example.com"
+          />
         </div>
+        <div className="inputGroup">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Your Password"
+          />
+        </div>
+        <button>Login</button>
       </div>
     </div>
   );
