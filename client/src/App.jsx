aed69b50859dec14b4bac853872079b642770e2c
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-22 19:28:48 +0800

    add router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,13 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        
+      </Router>
+    </div>
+  );
 }
 
 export default App;
