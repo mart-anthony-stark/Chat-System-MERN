756b82d618ecc1ac6f348b8e74f49e9661ab4f66
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-22 19:11:00 +0800

    add chatroom route

--- server/routes/chatroom.js
+++ server/routes/chatroom.js
@@ -5,5 +5,6 @@ const chatroomController = require("../controllers/chatroomController");
 const auth = require("../middlewares/auth");
 
 router.post("/", auth, catchErrors(chatroomController.createChatroom));
+router.get("/", auth, catchErrors(chatroomController.getAllChatrooms));
 
 module.exports = router;
