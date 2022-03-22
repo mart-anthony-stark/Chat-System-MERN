895528af6172ab1c3ddab8e8258de0b392800e9b
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-22 19:19:27 +0800

    remove

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -27,7 +25,7 @@ function App() {
           >
             Learn React
           </a>
-          {' | '}
+          {" | "}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
@@ -39,7 +37,7 @@ function App() {
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
