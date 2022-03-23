c77c9dc16e37e232dd5292b00baa0f685f76286a
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-23 15:12:22 +0800

    add cors

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
