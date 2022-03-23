381f257ba48575f3989cc0d1f4e88394bf69a0fc
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-23 15:34:23 +0800

    add toaster

--- client/src/Toaster.jsx
+++ client/src/Toaster.jsx
@@ -1,21 +1,20 @@
 import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
-const Toast = Swal.mixin({
-  toast: true,
-  position: "top-end",
-  showConfirmButton: false,
-  timer: 3000,
-  timerProgressBar: true,
-  onOpen: (toast) => {
-    toast.addEventListener("mouseenter", Swal.stopTimer);
-    toast.addEventListener("mouseleave", Swal.resumeTimer);
-  },
-});
+const MySwal = withReactContent(Swal);
 
 const makeToast = (type, msg) => {
-  Toast.fire({
+  MySwal.fire({
     icon: type,
     title: msg,
+    footer: "Copyright 2018",
+    didOpen: () => {
+      // `MySwal` is a subclass of `Swal`
+      //   with all the same instance & static methods
+      MySwal.clickConfirm();
+    },
+  }).then(() => {
+    return MySwal.fire(<p>{msg}</p>);
   });
 };
 
