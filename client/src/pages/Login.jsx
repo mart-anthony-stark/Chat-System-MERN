094c41525828089cf8441e9a003f3d67ce7c0b7c
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-23 14:50:13 +0800

    moved dir

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,30 @@
+const Login = () => {
+  return (
+    <div className="card">
+      <div className="cardHeader">Login</div>
+      <div className="cardBody">
+        <div className="inputGroup">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="abc@example.com"
+          />
+        </div>
+        <div className="inputGroup">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Your Password"
+          />
+        </div>
+        <button>Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
