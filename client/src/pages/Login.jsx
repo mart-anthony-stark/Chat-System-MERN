443b8ef667c59eca9ad1d8af008dac8d4f7df6a5
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-24 13:54:41 +0800

    add refs

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,37 @@
+import {useRef} from 'react'
+
 const Login = () => {
+
+  const handleRegister = () => {
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    axios
+      .post("http://localhost:8080/auth/register", {
+        name,
+        email,
+        password,
+      })
+      .then((res) => {
+        toast.success(res.data.message);
+        navigate("/login");
+      })
+      .catch((error) => {
+        if (error.response) {
+          // Request made and server responded
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+          toast.error(error.response.data.message);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+      });
+  };
+
   return (
     <div className="card">
       <div className="cardHeader">Login</div>
