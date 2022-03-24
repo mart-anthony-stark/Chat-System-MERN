16783e62701e27b497d8cad6c44e4b3785e6170a
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-24 14:49:30 +0800

    add connection

--- client/src/pages/Chatroom.jsx
+++ client/src/pages/Chatroom.jsx
@@ -1,13 +1,19 @@
 import { useParams, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import io from "socket.io-client";
 
 const Chatroom = () => {
   const { id } = useParams();
-  const socket = io("http://localhost:8080", {
-    query: {
-      token: localStorage.getItem("CC_Token"),
-    },
-  });
+  useEffect(() => {
+    const socket = io("http://localhost:8080", {
+      query: {
+        token: localStorage.getItem("CC_Token"),
+      },
+    });
+    return () => {
+      socket.off("message:new");
+    };
+  }, []);
 
   return <div>Chatroom Page</div>;
 };
