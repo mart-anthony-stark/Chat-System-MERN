cee2fd908783c29dd6e791ec69ab94e3a4d91499
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-24 14:04:11 +0800

    remove extra logs

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -25,9 +25,6 @@ const Login = () => {
       .catch((error) => {
         if (error.response) {
           // Request made and server responded
-          // console.log(error.response.data);
-          // console.log(error.response.status);
-          // console.log(error.response.headers);
           toast.error(error.response.data.message);
         } else if (error.request) {
           // The request was made but no response was received
