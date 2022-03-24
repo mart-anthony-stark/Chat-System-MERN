844bf821aab2577b83ccb809506f731bf173698b
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-24 14:09:29 +0800

    add route guard

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
+const IndexPage = () => {
+  const navigate = useNavigate();
+  React.useEffect(() => {
+    const token = localStorage.getItem("CC_Token");
+    if (!token) {
+      navigate("/login");
+    } else {
+      navigate("/dashboard");
+    }
+  }, []);
   return <div>Home Page</div>;
 };
 
-export default Login;
+export default IndexPage;
