a01755e83f4d74f7848b0556f0e6b87a64f8ee13
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-24 14:30:47 +0800

    add socket

--- client/src/pages/Chatroom.jsx
+++ client/src/pages/Chatroom.jsx
@@ -1,9 +1,14 @@
 import { useParams, useLocation } from "react-router-dom";
+import io from "socket.io-client";
 
 const Chatroom = () => {
-  const location = useLocation();
   const { id } = useParams();
-  console.log(id);
+  const socket = io("http//localhost:8080", {
+    query: {
+      token: localStorage.getItem("CC_Token"),
+    },
+  });
+
   return <div>Chatroom Page</div>;
 };
 
