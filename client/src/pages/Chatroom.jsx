026bdfabf5f7ffcb90409bde1065ded8f28fd602
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-24 14:27:49 +0800

    add socket io client pacjage

--- client/src/pages/Chatroom.jsx
+++ client/src/pages/Chatroom.jsx
@@ -1,6 +1,7 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 const Chatroom = () => {
+  const location = useLocation();
   const { id } = useParams();
   console.log(id);
   return <div>Chatroom Page</div>;
