8df7e191a163ec2cb88e3b18a8b386ea4a9af164
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-26 19:16:57 +0800

    add chatroom ui

--- client/src/pages/Chatroom.jsx
+++ client/src/pages/Chatroom.jsx
@@ -1,11 +1,32 @@
 import { useParams, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 
-const Chatroom = () => {
+const Chatroom = ({ socket }) => {
   const { id } = useParams();
-  
 
-  return <div>Chatroom Page</div>;
+  return (
+    <div className="chattroomPage">
+      <div className="chatroomSection">
+        <div className="cardHeader">Chatroom Page</div>
+        <div className="chatroomContent">
+          <div className="message">
+            <span className="otherMessage">Kit:</span> Hello Guys
+          </div>
+          <div className="message">
+            <span className="ownMessage">Mart:</span> Hello kit
+          </div>
+        </div>
+        <div className="chatroomActions">
+          <div>
+            <input type="text" name="message" placeholder="Say something..." />
+          </div>
+          <div>
+            <div className="join">Send</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chatroom;
