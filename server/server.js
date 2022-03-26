8e22b47425c503ed7e4e40d15b7181626ee122fd
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-26 19:46:26 +0800

    add message to db

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const app = require("./app");
 const mongoose = require("mongoose");
+const Message = require("./models/Message");
+const User = require("./models/User");
 
 const PORT = process.env.PORT || 8080;
 const server = app.listen(PORT, async () => {
@@ -35,7 +37,29 @@ io.on("connection", (socket) => {
     console.log("Disconnected " + socket.userId);
   });
 
-  socket.on("joinRoom", (props) => {
-    console.log(props);
+  socket.on("joinRoom", ({ chatroomId }) => {
+    socket.join(chatroomId);
+    console.log("Joined: " + chatroomId);
+  });
+
+  socket.on("leaveRoom", ({ chatroomId }) => {
+    socket.leave(chatroomId);
+    console.log("Left: " + chatroomId);
+  });
+
+  socket.on("chatroomMessage", async ({ chatroomId, message }) => {
+    if (message.trim().length > 0) {
+      const user = await User.findOne({ _id: socket.userId });
+      const newMessage = new Message({
+        chatroom: chatroomId,
+        user: socket.userId,
+        message,
+      });
+      io.to(chatroomId).emit("newMessage", {
+        message,
+        name: user.name,
+        user: socket.userId,
+      });
+    }
   });
 });
